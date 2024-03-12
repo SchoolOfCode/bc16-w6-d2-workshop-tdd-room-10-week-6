87d1d4009228fa3d459149e999c2befc7dd9a041
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-03-12 15:51:37 +0000

    ticket 4 done

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,46 +1,47 @@
 export function calculateScrabbleScore(word) {
-  const upperCaseWord = word.toUpperCase();
-  //object of key value pairs
-  const letterValues = {
-    A: 1,
-    B: 3,
-    C: 3,
-    D: 2,
-    E: 1,
-    F: 4,
-    G: 2,
-    H: 4,
-    I: 1,
-    J: 8,
-    K: 5,
-    L: 1,
-    M: 3,
-    N: 1,
-    O: 1,
-    P: 3,
-    Q: 10,
-    R: 1,
-    S: 1,
-    T: 1,
-    U: 1,
-    V: 4,
-    W: 4,
-    X: 8,
-    Y: 4,
-    Z: 10,
-  };
+	const upperCaseWord = word.toUpperCase();
+	//object of key value pairs
+	const letterValues = {
+		A: 1,
+		B: 3,
+		C: 3,
+		D: 2,
+		E: 1,
+		F: 4,
+		G: 2,
+		H: 4,
+		I: 1,
+		J: 8,
+		K: 5,
+		L: 1,
+		M: 3,
+		N: 1,
+		O: 1,
+		P: 3,
+		Q: 10,
+		R: 1,
+		S: 1,
+		T: 1,
+		U: 1,
+		V: 4,
+		W: 4,
+		X: 8,
+		Y: 4,
+		Z: 10,
+	};
+	if (!/^[a-zA-Z]*$/.test(word)) {
+		throw new Error(`unsupported characters in ${word}`);
+	}
+	// const value = letterValues[`${word}`];
+	// return value;
+	let score = 0;
 
-  // const value = letterValues[`${word}`];
-  // return value;
-  let score = 0;
-
-  //loop over each character in string word
-  for (let i = 0; i < upperCaseWord.length; i++) {
-    // match each char in word to letterValues object
-    console.log(upperCaseWord[i]);
-    score += letterValues[upperCaseWord[i]];
-  }
-  return score;
+	//loop over each character in string word
+	for (let i = 0; i < upperCaseWord.length; i++) {
+		// match each char in word to letterValues object
+		score += letterValues[upperCaseWord[i]];
+	}
+	return score;
 }
 //increment score variable by the letter score
 //return score
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -2,53 +2,63 @@ import { calculateScrabbleScore } from "./scrabble-score";
 import { expect, test, assert } from "vitest";
 // calculateScrabbleScore()
 test.each([
-  ["A", 1],
-  ["B", 3],
-  ["C", 3],
-  ["D", 2],
-  ["E", 1],
-  ["F", 4],
-  ["G", 2],
-  ["H", 4],
-  ["I", 1],
-  ["J", 8],
-  ["K", 5],
-  ["L", 1],
-  ["M", 3],
-  ["N", 1],
-  ["O", 1],
-  ["P", 3],
-  ["Q", 10],
-  ["R", 1],
-  ["S", 1],
-  ["T", 1],
-  ["U", 1],
-  ["V", 4],
-  ["W", 4],
-  ["X", 8],
-  ["Y", 4],
-  ["Z", 10],
+	["A", 1],
+	["B", 3],
+	["C", 3],
+	["D", 2],
+	["E", 1],
+	["F", 4],
+	["G", 2],
+	["H", 4],
+	["I", 1],
+	["J", 8],
+	["K", 5],
+	["L", 1],
+	["M", 3],
+	["N", 1],
+	["O", 1],
+	["P", 3],
+	["Q", 10],
+	["R", 1],
+	["S", 1],
+	["T", 1],
+	["U", 1],
+	["V", 4],
+	["W", 4],
+	["X", 8],
+	["Y", 4],
+	["Z", 10],
 ])(`%s should return %i`, (letter, expected) => {
-  expect(calculateScrabbleScore(letter)).toBe(expected);
+	expect(calculateScrabbleScore(letter)).toBe(expected);
 });
 
 test(`dog should equal 5`, () => {
-  //arrange
-  const word = "dog";
-  const expected = 5;
-  //act
-  const actual = calculateScrabbleScore(word);
-  //assert
-  expect(actual).toBe(expected);
+	//arrange
+	const word = "dog";
+	const expected = 5;
+	//act
+	const actual = calculateScrabbleScore(word);
+	//assert
+	expect(actual).toBe(expected);
 });
 
 test.each([
-  ["DOG", 5],
-  ["BOG", 6],
-  ["JOG", 11],
-  ["QUEEN", 14],
-  ["JAVASCRIPT", 24],
-  ["THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG", 99],
+	["DOG", 5],
+	["BOG", 6],
+	["JOG", 11],
+	["QUEEN", 14],
+	["JAVASCRIPT", 24],
+	["THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG", 99],
 ])(`%s should return %i`, (letter, expected) => {
-  expect(calculateScrabbleScore(letter)).toBe(expected);
+	expect(calculateScrabbleScore(letter)).toBe(expected);
+});
+
+test.each([
+	{ word: "fr0g", expected: /unsupported/ },
+	{ word: "99", expected: /unsupported/ },
+	{ word: 99, expected: /unsupported/ },
+	{ word: "£hello", expected: /unsupported/ },
+	{ word: "hello world", expected: /unsupported/ },
+])("should throw error if $word not A-Z a-z ", (word, expected) => {
+	expect(() => calculateScrabbleScore(word)).toThrowError(expected);
 });
